c3801f9dd8f295072f4fd80f50b129b98c8c83c1
Yulian302 <bohomolyulian3022003@gmail.com>
2025-03-29 10:51:28 -0400

    (fix): 2685. Count the Number of Complete Components

--- problems/graphs/bfs/2685. Count the Number of Complete Components.py
+++ problems/graphs/bfs/2685. Count the Number of Complete Components.py
@@ -1,3 +1,20 @@
+# https://leetcode.com/problems/count-the-number-of-complete-components
+# Difficulty: Medium
+# Tags: graph bfs complete_components
+
+# Problem
+# You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting vertices ai and bi.
+
+# Return the number of complete connected components of the graph.
+
+# A connected component is a subgraph of a graph in which there exists a path between any two vertices, and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+
+# A connected component is said to be complete if there exists an edge between every pair of its vertices.
+
+# Solution
+# Time O(V+E) and space O(V)
+# We use bfs to count the number of nodes and edges in a component. The thing is, the component is complete if the number of edges is equal to the number of nodes-1. Still for counting edges in bfs we should divide it by 2 in the end as each edge is visited twice. To compare the number of nodes and edges we should do the following comparison: (nodes * (nodes-1)) // 2 == edges
+
 from collections import defaultdict, deque
 
 
@@ -6,7 +23,7 @@ class Solution:
     def countCompleteComponents(self, n: int, edges: list[(int, int)]):
         graph = defaultdict(list)
         # for undirected graph
-        for x, y in edges:
+        for x, y in edges:  # O(N)
             graph[x].append(y)
             graph[y].append(x)
 
