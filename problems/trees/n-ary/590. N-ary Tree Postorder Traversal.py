90b55b0e7783165f9a206930de2c4849c4973f74
Yulian302 <bohomolyulian3022003@gmail.com>
2025-04-06 12:58:30 -0400

    590. N-ary Tree Postorder Traversal

--- problems/trees/n-ary/590. N-ary Tree Postorder Traversal.py
+++ problems/trees/n-ary/590. N-ary Tree Postorder Traversal.py
@@ -1,16 +1,17 @@
-# https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+# https://leetcode.com/problems/n-ary-tree-postorder-traversal/
 # Difficulty: Easy
-# Tags: tree n-ary preorder_traversal preorder
+# Tags: tree n-ary postorder_traversal postorder
 
 # Problem
-# Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+# Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
 
 # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
 # Solution
 # Recursive: Time O(N) and space O(N).
-# Iterative: Time O(N) and space O(N) worst when tree is imbalanced.
-# The idea behind is same as for binary trees, but here we have children as a list of children instead of left and right children. For iterative approach, stack pushing must be reversed to maintain natural order of tree nodes in preorder traversal.
+# Iterative: Time O(N) as reversing in a loop takes AT MOST O(N) [amortized] and space O(N).
+# The idea behind is same as for binary trees, but here we have children as a list of children instead of left and right children.
+
 
 """
 # Definition for a Node.
@@ -24,33 +25,37 @@ class Node:
 from typing import List
 
 
-def preorder(self, root: "Node") -> List[int]:
-    if root is None:
+def postorder(root: "Node") -> List[int]:  # type: ignore
+    if not root:
         return []
-
     res = []
     stack = [root]
     while stack:
         node = stack.pop()
         res.append(node.val)
-        stack.extend(node.children[::-1])
+        for child in node.children:
+            stack.append(child)
+
+    res.reverse()
     return res
 
 
 # --- or ---
 
 # recursive
-
-def preorder(self, root: 'Node') -> List[int]:
-    res = []
+def postorder(root: 'Node') -> List[int]:  # type: ignore
+    if not root:
+        return []
 
     def helper(node):
         if not node:
             return
 
-        res.append(node.val)
         for child in node.children:
             helper(child)
 
+        res.append(node.val)
+
+    res = []
     helper(root)
     return res
