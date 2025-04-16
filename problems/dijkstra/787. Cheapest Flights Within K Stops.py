f18422d72ab1139ecdd12a0c8cb3c952c44b4f0a
Yulian302 <bohomolyulian3022003@gmail.com>
2025-04-16 17:28:01 -0400

    787: Bellman-Ford algorithm

--- problems/dijkstra/787. Cheapest Flights Within K Stops.py
+++ problems/dijkstra/787. Cheapest Flights Within K Stops.py
@@ -11,8 +11,10 @@
 # Time O(V+ElogV) and space O(V+E)
 # We use dijkstra algorithm to find the cheapest flights from src to dest. But we need to keep track of the number of stops (k). So, our min heap internal logic is a bit different and has some additional if statements. Distances is an array of type [cost, stops], min heap is of type [cost, node, stops]. In this problem, dist = cost.
 
+from cmath import inf
 from collections import defaultdict
 from heapq import heappop, heappush
+from typing import List
 
 
 def findCheapestPrice(n: int, flights, src: int, dst: int, k: int
@@ -41,3 +43,30 @@ def findCheapestPrice(n: int, flights, src: int, dst: int, k: int
                 distances[nei] = [new_cost, stops + 1]
                 heappush(min_heap, (new_cost, nei, stops + 1))
     return -1
+
+
+# --- or ---
+# Bellman-Ford algorithm
+# Two arrays are used to keep track of prev distances and current distances. Edge relaxation is used to update with more optimal routes. This algorithm is good for finding shortest distance (or cheapest path) from source to destination with at most K edges.
+
+class Solution:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        if src == dst:
+            return 0
+
+        prev = [inf] * n
+        curr = [inf] * n
+        prev[src] = 0
+
+        for i in range(1, k + 2):
+            curr[src] = 0
+
+            for from_, to_, price in flights:
+                if prev[from_] < inf:
+                    curr[to_] = min(curr[to_], prev[from_] + price)
+
+            prev = curr[:]
+
+        return curr[dst] if curr[dst] != inf else -1
